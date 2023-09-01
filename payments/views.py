640b7a0de8072103289f1f7a52da4f4ad14f6d8e
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-09-01 10:15:38 -0300

    Added bill as payment method.

--- payments/views.py
+++ payments/views.py
@@ -19,7 +19,36 @@ def checkout(request):
 
 
 def methodsPayments(request):
-    print(sdk)
     payment_methods_response = sdk.payment_methods().list_all()
     payment_methods = payment_methods_response["response"]
     return JsonResponse(payment_methods, safe=False)
+
+
+def methodTicket(request):
+    payment_data = {
+        "transaction_amount": 100,
+        "description": "Título do produto",
+        # pec = Boleto Loterica, bolbradesco = Boleto Bradesco
+        "payment_method_id": "bolbradesco",
+        "payer": {
+            "email": "UserTest@gmail.com",
+            "first_name": "Test",
+            "last_name": "User",
+            "identification": {
+                "type": "CPF",
+                "number": "191191191-00"
+            },
+            "address": {
+                "zip_code": "06233-200",
+                "street_name": "Av. das Nações Unidas",
+                "street_number": "3003",
+                "neighborhood": "Bonfim",
+                "city": "Osasco",
+                "federal_unit": "SP"
+            }
+        }
+    }
+
+    payment_response = sdk.payment().create(payment_data)
+    payment = payment_response["response"]
+    return JsonResponse(payment, safe=False)
