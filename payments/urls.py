dcca9bdf8b1ae5312668d6ca898594dbab7df6cf
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-09-05 13:09:35 -0300

    Removing urls from API mercado pago .

--- payments/urls.py
+++ payments/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('payments-confirm/', views.paymentsConfirm, name='paymentsConfirm'),
     path('payment/', views.payment, name='payment'),
-    path('methods-payments/', views.methodsPayments, name='methodsPayments'),
-    path('method-ticket/', views.methodTicket, name='methodTicket'),
-    path('method-pix/', views.methodPix, name='methodPix'),
 ]
